97a72112f36d4afeb8edc3ef0750820830124180
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2019-07-02 11:44:57 -0400

    Rework package installation tests to be more precise
    Now the test makes sure that all packages were installed for a
    particular distribution are indeed installed.

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,7 +1,6 @@
 """Module containing the tests for the default scenario."""
 
 import os
-import pytest
 
 import testinfra.utils.ansible_runner
 
@@ -10,17 +9,17 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 ).get_hosts("all")
 
 
-@pytest.mark.parametrize(
-    "pkgs",
-    [
-        ["python-pip", "python27-pip"],
-        ["python3-pip", "python36-pip"],
-        ["python-dev", "python27-devel"],
-        ["python3-dev", "python36-devel"],
-    ],
-)
-def test_pip(host, pkgs):
+def test_pip(host):
     """Test that the appropriate pip packages were installed."""
+    if host.system_info.distribution == "debian":
+        pkgs = ["python-pip", "python-dev", "python3-pip", "python3-dev"]
+    elif host.system_info.distribution == "fedora":
+        pkgs = ["python2-pip", "python2-devel", "python3-pip", "python3-devel"]
+    elif host.system_info.distribution == "amzn":
+        pkgs = ["python27-pip", "python27-devel", "python36-pip", "python36-devel"]
+    else:
+        pkgs = []
     packages = [host.package(pkg) for pkg in pkgs]
     installed = [package.is_installed for package in packages]
-    assert any(installed)
+    assert len(pkgs) != 0
+    assert all(installed)
