60d56087ae9a7f7fab44199d00b4a01a5a347980
Nicholas McDonnell <50747025+mcdonnnj@users.noreply.github.com>
2022-04-14 18:09:32 -0400

    Update testing for pip on Debian Buster
    This includes streamlining tests a bit and using the distribution
    codename to identify Stretch and Buster.

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -12,29 +12,29 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 ).get_hosts("all")
 
 
-@pytest.mark.parametrize("pkg", ["python3-pip", "python3-dev"])
+@pytest.mark.parametrize("pkg", ["python-pip", "python-dev"])
 def test_python_debian(host, pkg):
-    """Test that the appropriate packages were installed."""
-    if host.system_info.distribution == "debian" and host.system_info.release != "9.12":
+    """Test that the appropriate packages were installed.
+
+    We treat Debian 9 and 10 as special cases because the CyHy AMIs (and
+    only the CyHy AMIs) are built on them.  Therefore they require
+    python2.
+    """
+    if host.system_info.distribution == "debian" and (
+        host.system_info.codename == "stretch" or host.system_info.codename == "buster"
+    ):
         assert host.package(pkg).is_installed
 
 
-@pytest.mark.parametrize(
-    "pkg", ["python-pip", "python-dev", "python3-pip", "python3-dev"]
-)
-def test_python_debian_9(host, pkg):
-    """Test that the appropriate packages were installed.
-
-    We treat Debian 9 as a special case because the CyHy AMIs (and
-    only the CyHy AMIs) are built on it.  Therefore it requires
-    python2.
-    """
-    if host.system_info.distribution == "debian" and host.system_info.release == "9.12":
+@pytest.mark.parametrize("pkg", ["python3-pip", "python3-dev"])
+def test_python3_debian(host, pkg):
+    """Test that the appropriate packages were installed."""
+    if host.system_info.distribution == "debian":
         assert host.package(pkg).is_installed
 
 
 @pytest.mark.parametrize("pkg", ["python3-pip", "python3-devel"])
-def test_python_redhat(host, pkg):
+def test_python3_redhat(host, pkg):
     """Test that the appropriate packages were installed."""
     if (
         host.system_info.distribution == "fedora"
