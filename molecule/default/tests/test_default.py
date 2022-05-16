d12160ad0ad40399d6d4e53f89a9de0f565ba12d
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2022-05-16 14:11:27 -0400

    Ensure Python 2 packages are installed on Amazon Linux 2 and update tests
    Amazon Linux 2 requires Python 2, so it should be treated just like
    Debian Stretch.
    
    Also refactor the molecule test code.  The previous code would pass if
    the distro wasn't one that was covered by the tests, while this code
    will fail in such a case.

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -4,7 +4,6 @@
 import os
 
 # Third-Party Libraries
-import pytest
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
@@ -12,32 +11,24 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 ).get_hosts("all")
 
 
-@pytest.mark.parametrize("pkg", ["python3-pip", "python3-dev"])
-def test_python_debian(host, pkg):
+def test_pip(host):
     """Test that the appropriate packages were installed."""
-    if host.system_info.distribution == "debian" and host.system_info.release != "9.12":
-        assert host.package(pkg).is_installed
-
-
-@pytest.mark.parametrize(
-    "pkg", ["python-pip", "python-dev", "python3-pip", "python3-dev"]
-)
-def test_python_debian_9(host, pkg):
-    """Test that the appropriate packages were installed.
-
-    We treat Debian 9 as a special case because the CyHy AMIs (and
-    only the CyHy AMIs) are built on it.  Therefore it requires
-    python2.
-    """
-    if host.system_info.distribution == "debian" and host.system_info.release == "9.12":
-        assert host.package(pkg).is_installed
-
-
-@pytest.mark.parametrize("pkg", ["python3-pip", "python3-devel"])
-def test_python_redhat(host, pkg):
-    """Test that the appropriate packages were installed."""
-    if (
-        host.system_info.distribution == "fedora"
-        or host.system_info.distribution == "amzn"
-    ):
-        assert host.package(pkg).is_installed
+    amazon_extra_pkgs = ["python2-pip", "python-devel"]
+    debian_pkgs = ["python3-pip", "python3-dev"]
+    debian_stretch_extra_pkgs = ["python-pip", "python-dev"]
+    redhat_pkgs = ["python3-pip", "python3-devel"]
+    if host.system_info.distribution in ["debian", "kali", "ubuntu"]:
+        assert all([host.package(pkg).is_installed for pkg in debian_pkgs])
+        if (
+            host.system_info.distribution == "debian"
+            and host.system_info.codename == "9.12"
+        ):
+            assert all(
+                [host.package(pkg).is_installed for pkg in debian_stretch_extra_pkgs]
+            )
+    elif host.system_info.distribution in ["amzn", "fedora"]:
+        assert all([host.package(pkg).is_installed for pkg in redhat_pkgs])
+        if host.system_info.distribution == "amzn":
+            assert all([host.package(pkg).is_installed for pkg in amazon_extra_pkgs])
+    else:
+        assert False, f"Unknown distribution {host.system_info.distribution}"
