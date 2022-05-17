34fc1215c9db185e5b5926875e92b1ec67736ba1
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2022-05-17 11:43:55 -0400

    Separate the pip2 and pip3 code into two separate tests
    Co-authored-by: Nick <50747025+mcdonnnj@users.noreply.github.com>

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -11,14 +11,11 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 ).get_hosts("all")
 
 
-def test_pip(host):
-    """Test that the appropriate packages were installed."""
-    amazon_extra_pkgs = ["python2-pip", "python-devel"]
-    debian_pkgs = ["python3-pip", "python3-dev"]
-    debian_stretch_extra_pkgs = ["python-pip", "python-dev"]
-    redhat_pkgs = ["python3-pip", "python3-devel"]
+def test_pip2(host):
+    """Test that the appropriate pip2 packages were installed."""
+    amazon_pkgs = ["python2-pip", "python-devel"]
+    debian_stretch_pkgs = ["python-pip", "python-dev"]
     if host.system_info.distribution in ["debian", "kali", "ubuntu"]:
-        assert all([host.package(pkg).is_installed for pkg in debian_pkgs])
         # We treat Debian 9 as a special case because the CyHy AMIs
         # (and only the CyHy AMIs) are built on it.  Therefore it
         # requires python2.
@@ -26,15 +23,24 @@ def test_pip(host):
             host.system_info.distribution == "debian"
             and host.system_info.codename == "stretch"
         ):
-            assert all(
-                [host.package(pkg).is_installed for pkg in debian_stretch_extra_pkgs]
-            )
+            assert all([host.package(pkg).is_installed for pkg in debian_stretch_pkgs])
     elif host.system_info.distribution in ["amzn", "fedora"]:
-        assert all([host.package(pkg).is_installed for pkg in redhat_pkgs])
         # Amazon Linux 2 is woefully behind the times and requires
         # Python 2 to support its antiquated version of the yum
         # package manager.
         if host.system_info.distribution == "amzn":
-            assert all([host.package(pkg).is_installed for pkg in amazon_extra_pkgs])
+            assert all([host.package(pkg).is_installed for pkg in amazon_pkgs])
+    else:
+        assert False, f"Unknown distribution {host.system_info.distribution}"
+
+
+def test_pip3(host):
+    """Test that the appropriate pip3 packages were installed."""
+    debian_pkgs = ["python3-pip", "python3-dev"]
+    redhat_pkgs = ["python3-pip", "python3-devel"]
+    if host.system_info.distribution in ["debian", "kali", "ubuntu"]:
+        assert all([host.package(pkg).is_installed for pkg in debian_pkgs])
+    elif host.system_info.distribution in ["amzn", "fedora"]:
+        assert all([host.package(pkg).is_installed for pkg in redhat_pkgs])
     else:
         assert False, f"Unknown distribution {host.system_info.distribution}"
