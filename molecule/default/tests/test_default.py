f62bbfac45d18a82cceff7add079f635483585ba
David Redmin <david.redmin@gwe.cisa.dhs.gov>
2026-06-03 10:43:51 -0400

    Add a new test to check that the version of pip3 installed is new enough to support pyproject.toml (pip3 >= 19.0)

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -25,3 +25,12 @@ def test_pip3(host):
         assert all(host.package(pkg).is_installed for pkg in redhat_pkgs)
     else:
         raise AssertionError(f"Unknown distribution {host.system_info.distribution}")
+
+
+# Test that pip3 version is at least 19.0, which is the minimum version that
+# supports pyproject.toml (required for installing some packages).
+def test_pip3_version(host):
+    """Test that pip3 version is at least 19.0."""
+    pip3_version = host.check_output("pip3 --version").split()[1]
+    major, minor, *_ = pip3_version.split(".")
+    assert int(major) >= 19, f"pip3 major version {major} is less than 19"
